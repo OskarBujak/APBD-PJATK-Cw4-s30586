c6f6b3ae1c71d3a7d16fc37a6f086863c539ea64
OskarBujak <103437777+OskarBujak@users.noreply.github.com>
2026-05-25 20:28:54 +0200

    klasy

---
+++ APBD-PJATK-Cw4-s30586/Component.cs
@@ -0,0 +1,14 @@
+﻿public class Component
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+
+    public int ComponentManufacturersId { get; set; }
+    public ComponentManufacturer ComponentManufacturer { get; set; } = null!;
+
+    public int ComponentTypesId { get; set; }
+    public ComponentType ComponentType { get; set; } = null!;
+
+    public ICollection<PCComponent> PCComponents { get; set; } = new List<PCComponent>();
+}
