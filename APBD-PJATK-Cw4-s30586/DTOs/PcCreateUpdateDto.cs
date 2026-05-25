1e6435644c59b5c4d0cec04528fd21afc91ca0c6
OskarBujak <103437777+OskarBujak@users.noreply.github.com>
2026-05-25 22:00:46 +0200

    fixes

--- APBD-PJATK-Cw4-s30586/DTOs/PcCreateUpdateDto.cs
+++ APBD-PJATK-Cw4-s30586/DTOs/PcCreateUpdateDto.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 public class PcCreateUpdateDto
 {
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(50, ErrorMessage = "Name can be maximum of 50 characters.")]
     public string Name { get; set; } = null!;
+
+    [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Must be greater than 0.01")]
     public float Weight { get; set; }
+
+    [Required]
+    [Range(0, 120, ErrorMessage = "Must be between 0 and 120.")]
     public int Warranty { get; set; }
+
+    [Required]
     public DateTime CreatedAt { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "Can't be negative.")]
     public int Stock { get; set; }
 }
